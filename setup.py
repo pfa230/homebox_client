24491e969ef5b590441b55a1857a8f4dacf6466f
pfa230 <pfa230@pfa.name>
2025-12-29 17:59:10 -0500

    Fix version setting logic

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 """
     Homebox API
 
@@ -11,57 +10,8 @@
     Do not edit the class manually.
 """  # noqa: E501
 
+from setuptools import setup
 
-from setuptools import setup, find_packages  # noqa: H301
 
-try:
-    from setuptools_scm import get_version
-except Exception:  # pragma: no cover - fallback for legacy installs
-    get_version = None
-
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
-NAME = "homebox-client"
-VERSION = get_version(root=".", relative_to=__file__) if get_version else "0.0.0"
-PYTHON_REQUIRES = ">= 3.9"
-REQUIRES = [
-    "attrs >= 22.2.0",
-    "httpx >= 0.23.0, < 0.29.0",
-]
-CLASSIFIERS = [
-    "Development Status :: 4 - Beta",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Programming Language :: Python :: 3.12",
-    "Programming Language :: Python :: 3.13",
-    "Typing :: Typed",
-]
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description="Homebox API client",
-    author="pfa230",
-    author_email="pfa230@pfa.name",
-    url="https://github.com/pfa230/homebox_client",
-    keywords=["OpenAPI", "openapi-python-client", "Homebox API"],
-    license="MIT",
-    classifiers=CLASSIFIERS,
-    install_requires=REQUIRES,
-    packages=find_packages(exclude=["test", "tests"]),
-    include_package_data=True,
-    long_description_content_type='text/markdown',
-    long_description="""\
-    Track, Manage, and Organize your Things.
-    """,  # noqa: E501
-    package_data={"homebox_client": ["py.typed", "*.pyi"]},
-)
+if __name__ == "__main__":
+    setup()
